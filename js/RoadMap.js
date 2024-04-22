a8cda7326026018f717c3a0ea2aeb73a51473dfd
Ahmad Zaidan <ahmad.zaidan8a8@gmail.com>
2024-04-22 22:31:13 +0200

    postion the elements on the roads

--- js/RoadMap.js
+++ js/RoadMap.js
@@ -1,5 +1,5 @@
 
-
+const ELEMENT_WIDTH = 50;
 class Road {
     constructor(startX , startY , endX , endY) {
         this.startX = startX;
@@ -10,6 +10,8 @@ class Road {
     this.distance = Math.sqrt(Math.pow(endX - startX, 2) + Math.pow(endY - startY, 2));
     // the angle between 2 points relative to the postive axis
     this.angle = Math.atan2(endY - startY, endX - startX) * 180 / Math.PI;
+    // to Keep track of the road that inserted dynamiclly
+    this.road = null ;
 
     }
 
@@ -36,30 +38,30 @@ class Road {
         road.style.top = Math.min(this.startY, this.endY) + "px";
     }
 
-
+    this.road = road;
 
     document.getElementById("road-container").appendChild(road);
     }
     // arr
     addElements(elments){
-        const elementsContainer = document.createElement('div');
-        elementsContainer.className = 'elments-container';
-        document.getElementById("road-container").appendChild(elementsContainer);
+        if(elments.length < 2)
+            throw new Error('at least two elemets are required.');
 
-        let spacing = this.distance / (elments.length + 1);
-        for(let i = 0 ; i < elments.length ; i++){
-            let elementDistance = (i + 1) * spacing;
-            let elementX = this.startX + (elementDistance * Math.cos(this.angle * Math.PI / 180));
-            let elementY = this.startY + (elementDistance * Math.sin(this.angle * Math.PI / 180));
+        let firstElmX = this.startX - ELEMENT_WIDTH;
+        let firstElmY = this.startY;
 
-            const element = new RoadElement(elementX,elementY);
-            elementsContainer.appendChild(element.element);
+        let secondElmX = this.endX;
+        let secondElmY = this.endY;
+
+        const firstElm = new RoadElement(firstElmX,firstElmY);
+        const secondElm = new RoadElement(secondElmX,secondElmY);
+            
+        document.getElementById('road-container').appendChild(firstElm.element);
+        document.getElementById('road-container').appendChild(secondElm.element);
         }
 
 
     }
-
-}
 
 class RoadElement {
     constructor(x, y) {
@@ -70,7 +72,7 @@ class RoadElement {
     }
 }
 
-
+// RoadMap Structure
 // Horz
 const road1 = new Road(100 ,130 , 500 ,130);
 const road2 = new Road(100 ,300 , 500 ,300);
@@ -79,10 +81,13 @@ const road3 = new Road(100 ,500 , 500 ,500);
 // Vertical
 const road4 = new Road(300 ,70 , 300 ,600);
 
-
 road1.createRoad();
 road2.createRoad();
 road3.createRoad();
 road4.createRoad();
 
-// road.addElements(['item1','item2','item3']);
+
+road1.addElements(['item1','item2']);
+road2.addElements(['item1','item2']);
+road3.addElements(['item1','item2']);
+road4.addElements(['item1','item2']);
